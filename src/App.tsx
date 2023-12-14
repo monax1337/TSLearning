a6a719ed4fe0d0078aa4ae5c849ae96b4ad3bc06
Kruglikovskiy Maxim <maxfff03@gmail.com>
2023-12-14 21:07:10 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Card, {CardVariant} from "./Components/Card";
-import UserList from "./Components/UserList";
-import {IUser} from "./types/types";
+import {ITodo, IUser} from "./types/types";
+import axios from "axios";
+import List from "./Components/List";
+import UserItem from "./Components/UserItem";
+import TodoItem from "./Components/TodoItem";
+import EventExample from "./Components/EventExample";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import UserPage from "./Components/UserPage";
+import TodosPage from "./Components/TodosPage";
+import UserItemPage from "./Components/UserItemPage";
+import TodoItemPage from "./Components/TodoItemPage";
 
 const App = () => {
-    const users: IUser[] = [
-        {
-            id: 1,
-            name: 'Monax',
-            email: 'maxfff03@gmail.com',
-            address: {city: 'Minsk', street: 'Polevaya', zipcode: '12345'}
-        },
-        {
-            id: 2,
-            name: 'Dimass',
-            email: 'dimassclod03@gmail.com',
-            address: {city: 'Minsk', street: 'Malina', zipcode: '3456789'}
-        }
-    ]
+
     return (
-        <div>
-            <Card height='200px' width='200px' variant={CardVariant.outlined}
-                  onClick={(num) => console.log('click', num)}>
-                <button>Кнопка</button>
-            </Card>
-            <UserList users={users}/>
-        </div>
+        <BrowserRouter>
+            <div>
+                <div>
+                    <Link to='/users'>Пользователи</Link>
+                    <Link to='/todos'>Список дел</Link>
+                </div>
+                <Routes>
+                    <Route path="/users" element={<UserPage/>}/>
+                    <Route path="/todos" element={<TodosPage/>}/>
+                    <Route path="/users/:id" element={<UserItemPage/>}/>
+                    <Route path="/todos/:id" element={<TodoItemPage/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
+
+        // <div>
+        //     <EventExample/>
+        //     <Card height='200px' width='200px' variant={CardVariant.outlined}
+        //           onClick={(num) => console.log('click', num)}>
+        //         <button>Кнопка</button>
+        //     </Card>
+        // </div>
     );
 };
 
